62c8db75b62410bedc9db0d6d95f3ee0976f6992
Daniel Tyson <danielctyson@gmail.com>
2015-04-20 17:58:31 -0400

    Added headshot for personal touch

--- app/scripts/controllers/FormCtrl.js
+++ app/scripts/controllers/FormCtrl.js
@@ -13,6 +13,7 @@ function formCtrl (FormFactory) {
         var vm = this;
 
         FormFactory.email(vm.email_params).then(function(){
+
             vm.email_params.email_body = null;
             vm.email_params.first_name = null;
             vm.email_params.last_name = null;
--- app/scripts/factories/FormFactory.js
+++ app/scripts/factories/FormFactory.js
@@ -5,6 +5,7 @@ angular
     .factory('FormFactory', ['$http', '$window', function($http, $window){
 
             var email = function(email_params) {
+                debugger
                 return $http.post('https://lit-hamlet-4007.herokuapp.com/email', email_params).success(function(response){
                     console.log('Sent!');
                 }).error(function(data,status,headers,config){
